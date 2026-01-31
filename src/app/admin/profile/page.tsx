c95599dcfdbb2d0ed01caf089cd3042675a95417
LiteralKrishu <sousnigdhodas@gmail.com>
2026-02-01 00:03:13 +0530

    Fix

--- src/app/admin/profile/page.tsx
+++ src/app/admin/profile/page.tsx
@@ -55,7 +55,7 @@ export default async function AdminProfilePage() {
     // Get initials for avatar
     const initials = profile.full_name
         .split(' ')
-        .map((n) => n[0])
+        .map((n: string) => n[0])
         .join('')
         .toUpperCase()
         .slice(0, 2);
